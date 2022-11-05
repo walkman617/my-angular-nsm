c0dd3336727a87c393a32251492d8fd743522223
Neo Shi <shiwenxuan2003@gmail.com>
2022-11-05 13:59:53 +0800

    Initialize Website

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-starter app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('《网络安全与管理》');
   });
 });
